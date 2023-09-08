61a27b8c738b7dc6146c5e05d768813ab49cfaa5
PALIKALA DIVYA MAHESWARI <119839678+divyamaheswari7@users.noreply.github.com>
2023-09-08 23:27:50 +0530

    #include <stdio.h>
    static int addDigits(int num) {
            return num - (num - 1) / 9 * 9;
        }
    
    int main(void)
    {
        int n;
        scanf("%d",&n);
        printf("%d",addDigits(n));
        return 0;
    }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+
+static int addDigits(int num) {
+        return num - (num - 1) / 9 * 9;
+    }
+
+int main(void)
 {
     int n;
     scanf("%d",&n);
-    if(n<=9)
-    printf("%d",n);
-    else if(n%9==0)
-    printf("9");
-    else printf("%d",n%9);
+    printf("%d",addDigits(n));
+    return 0;
 }
